cd5ce4a38bee039274494db656155f9c0bfa9a2b
anteater@mac <anteater1056@gmail.com>
2024-06-16 19:56:09 +0900

    로깅 테스트

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  /** 로깅 모듈 */
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
+    console.log('the application generated a message : Hello World!');
+    this.logger.log('Hello World!');
+    this.logger.debug('Hello World!');
+    this.logger.error('Hello World!');
+    this.logger.fatal('Hello World!');
+    this.logger.verbose('Hello World!');
+    this.logger.warn('Hello World!');
+
     return 'Hello World!';
   }
 }
